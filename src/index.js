6306e3a962d6c1c20a8783254ecd09e6b1557242
Christopher Alexander Trotter <chrisat@ifi.uio.no>
2018-03-03 20:23:17 +0100

    Initialized a React Project Skeleton
    Hopefully useful when wanting to start an empty future project.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//@flow
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Layout from './js/components/layout/layout' 
+
+const app = document.getElementById('root')
+
+ReactDOM.render(<Layout />, app)
